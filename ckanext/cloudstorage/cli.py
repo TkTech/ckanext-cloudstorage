a107dbf0b6925b67f69e4ca73c5961391fe1aa1c
Tyler Kennedy <tk@tkte.ch>
2016-06-24 13:47:03 -0400

    Add an experimental paster command to migrate local storage to the cloud.

--- ckanext/cloudstorage/cli.py
+++ ckanext/cloudstorage/cli.py
@@ -1,22 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+import os.path
+import cgi
+
 from docopt import docopt
 from ckan.lib.cli import CkanCommand
 
-from ckanext.cloudstorage.storage import CloudStorage
+from ckanapi import LocalCKAN
+from ckanext.cloudstorage.storage import (
+    CloudStorage,
+    ResourceCloudStorage
+)
 
 
 USAGE = """ckanext-cloudstorage
 
 Commands:
     - fix-cors       Update CORS rules where possible.
+    - migrate        Upload local storage to the remote.
 
 Usage:
     cloudstorage fix-cors <domains>... [--c=<config>]
+    cloudstorage migrate <path_to_storage> [--c=<config>]
 
 Options:
     -c=<config>       The CKAN configuration file.
 """
+
+
+class FakeFileStorage(cgi.FieldStorage):
+    def __init__(self, fp, filename):
+        self.file = fp
+        self.filename = filename
 
 
 class PasterCommand(CkanCommand):
@@ -28,31 +44,93 @@ class PasterCommand(CkanCommand):
         args = docopt(USAGE, argv=self.args)
 
         if args['fix-cors']:
-            cs = CloudStorage()
+            _fix_cors(args)
+        elif args['migrate']:
+            _migrate(args)
 
-            if cs.can_use_advanced_azure:
-                from azure.storage import blob as azure_blob
-                from azure.storage import CorsRule
 
-                blob_service = azure_blob.BlockBlobService(
-                    cs.driver_options['key'],
-                    cs.driver_options['secret']
-                )
+def _migrate(args):
+    path = args['<path_to_storage>']
+    if not os.path.isdir(path):
+        print('The storage directory cannot be found.')
+        return
 
-                blob_service.set_blob_service_properties(
-                    cors=[
-                        CorsRule(
-                            allowed_origins=args['<domains>'],
-                            allowed_methods=['GET']
-                        )
-                    ]
+    lc = LocalCKAN()
+    resources = {}
+
+    # The resource folder is stuctured like so on disk:
+    # - storage/
+    #   - ...
+    # - resources/
+    #   - <3 letter prefix>
+    #     - <3 letter prefix>
+    #       - <remaining resource_id as filename>
+    #       ...
+    #     ...
+    #   ...
+    for root, dirs, files in os.walk(path):
+        # Only the bottom level of the tree actually contains any files. We
+        # don't care at all about the overall structure.
+        if not files:
+            continue
+
+        split_root = root.split('/')
+        resource_id = split_root[-2] + split_root[-1]
+
+        for file_ in files:
+            resources[resource_id + file_] = os.path.join(
+                root,
+                file_
+            )
+
+    for i, resource in enumerate(resources.iteritems(), 1):
+        resource_id, file_path = resource
+        print('[{i}/{count}] Working on {id}'.format(
+            i=i,
+            count=len(resources),
+            id=resource_id
+        ))
+
+        resource = lc.action.resource_show(id=resource_id)
+        if resource['url_type'] != 'upload':
+            continue
+
+        with open(os.path.join(root, file_path), 'rb') as fin:
+            resource['upload'] = FakeFileStorage(
+                fin,
+                resource['url'].split('/')[-1]
+            )
+
+            uploader = ResourceCloudStorage(resource)
+            uploader.upload(resource['id'])
+
+
+def _fix_cors(args):
+    cs = CloudStorage()
+
+    if cs.can_use_advanced_azure:
+        from azure.storage import blob as azure_blob
+        from azure.storage import CorsRule
+
+        blob_service = azure_blob.BlockBlobService(
+            cs.driver_options['key'],
+            cs.driver_options['secret']
+        )
+
+        blob_service.set_blob_service_properties(
+            cors=[
+                CorsRule(
+                    allowed_origins=args['<domains>'],
+                    allowed_methods=['GET']
                 )
-                print('Done!')
-            else:
-                print(
-                    'The driver {driver_name} being used does not currently'
-                    ' support updating CORS rules through'
-                    ' cloudstorage.'.format(
-                        driver_name=cs.driver_name
-                    )
-                )
+            ]
+        )
+        print('Done!')
+    else:
+        print(
+            'The driver {driver_name} being used does not currently'
+            ' support updating CORS rules through'
+            ' cloudstorage.'.format(
+                driver_name=cs.driver_name
+            )
+        )
