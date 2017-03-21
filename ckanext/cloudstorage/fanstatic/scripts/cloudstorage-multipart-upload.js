db367ec4aae37769b56d446222fe07d1f9670bd1
Artem Bazykin <artem.bazykin@linkdigital.com.au>
2017-03-21 15:04:36 +0200

    SXOEHXZEB-16 / Dataset creation Previous button submits upload and redirects

--- ckanext/cloudstorage/fanstatic/scripts/cloudstorage-multipart-upload.js
+++ ckanext/cloudstorage/fanstatic/scripts/cloudstorage-multipart-upload.js
@@ -21,6 +21,7 @@ ckan.module('cloudstorage-multipart-upload', function($, _) {
         _uploadSize: null,
         _uploadName: null,
         _uploadedParts: null,
+        _btnClick: null,
 
         initialize: function() {
             $.proxyAll(this, /_on/);
@@ -241,13 +242,24 @@ ckan.module('cloudstorage-multipart-upload', function($, _) {
                 return;
             }
             event.preventDefault();
-
-            try{
-                this._onDisableSave(true);
-                this._onSaveForm();
-            } catch(error){
-                console.log(error);
+            var dataset_id = this.options.packageId;
+            this._btnClick = $(event.target).attr('value');
+            if (this._btnClick == 'go-dataset') {
                 this._onDisableSave(false);
+                var redirect_url = this.sandbox.url(
+                    '/dataset/edit/' +
+                    dataset_id);
+                window.setTimeout(function(){
+                    window.location = redirect_url;
+                }, 1000);
+            } else {
+                try{
+                    this._onDisableSave(true);
+                    this._onSaveForm();
+                } catch(error){
+                    console.log(error);
+                    this._onDisableSave(false);
+                }
             }
 
             // this._form.trigger('submit', true);
@@ -352,13 +364,17 @@ ckan.module('cloudstorage-multipart-upload', function($, _) {
 
         _onFinishUpload: function() {
             var self = this;
+            var data_dict = {
+                'uploadId': this._uploadId,
+                'id': this._resourceId,
+            }
+            if (self._btnClick == 'go-metadata') {
+                data_dict.finish = true
+            }
             this.sandbox.client.call(
                 'POST',
                 'cloudstorage_finish_multipart',
-                {
-                    'uploadId': this._uploadId,
-                    'id': this._resourceId
-                },
+                data_dict,
                 function (data) {
 
                     self._progress.hide('fast');
@@ -371,11 +387,17 @@ ckan.module('cloudstorage-multipart-upload', function($, _) {
                             'success'
                         );
                         // self._form.remove();
-                        var redirect_url = self.sandbox.url(
-                            '/dataset/' +
-                            self._packageId +
-                            '/resource/' +
-                            self._resourceId);
+                        if (self._btnClick == 'again') {
+                            var redirect_url = self.sandbox.url(
+                                '/dataset/new_resource/' +
+                                self._packageId
+                            );
+                        } else {
+                            var redirect_url = self.sandbox.url(
+                                '/dataset/' +
+                                self._packageId
+                            );
+                        }
                         self._form.attr('action', redirect_url);
                         self._form.attr('method', 'GET');
                         self.$('[name]').attr('name', null);
