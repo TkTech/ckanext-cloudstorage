83cde5881857f431a98d371b5b33881ee4fe8f50
ccancellieri <ccancellieri@hotmail.com>
2024-01-31 11:38:14 +0100

    fix upload

--- ckanext/cloudstorage/bucket.py
+++ ckanext/cloudstorage/bucket.py
@@ -5,7 +5,6 @@ from google.cloud import storage
 from google.cloud.exceptions import NotFound, GoogleCloudError
 
 
-
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -14,6 +13,7 @@ log = logging.getLogger(__name__)
 class UploadError(Exception):
     """Custom exception for upload failures."""
     pass
+
 
 class BucketError(Exception):
     """Custom exception for upload failures."""
@@ -45,11 +45,13 @@ def create_bucket(bucket_name, cloud_storage=None):
         log.error("Error creating bucket: {}".format(e))
         raise BucketError("Error creating bucket: {}".format(e))
 
+
 def check_err_response_from_gcp(response, err_msg):
     if "error" in response:
         log.error("{}: {}".format(err_msg, response))
-        raise  Exception(response["error"])
+        raise Exception(response["error"])
     return response
+
 
 def add_group_iam_permissions(bucket_name, group_email):
     """
@@ -71,18 +73,21 @@ def add_group_iam_permissions(bucket_name, group_email):
         raise RuntimeError("Bucket '{}' not found.".format(bucket_name))
     except Exception as e:
         # This block will execute for any other exceptions
-        raise RuntimeError("An error occurred getting bucket info: {}".format(e))
+        raise RuntimeError(
+            "An error occurred getting bucket info: {}".format(e))
 
     policy = bucket.get_iam_policy()
     response = check_err_response_from_gcp(policy, "Error getting Iam policiy")
     log.info("Iam policy {}".format(response))
-    
+
     viewer_role = "roles/storage.objectViewer"
     policy[viewer_role].add("group:" + group_email)
     response = bucket.set_iam_policy(policy)
-    response = check_err_response_from_gcp(response, "Error modifying bucket IAM policy")
+    response = check_err_response_from_gcp(
+        response, "Error modifying bucket IAM policy")
     log.info("Read and list permissions granted to group {} on bucket {}:  IAM Policy is now:\n{}"
-                .format(group_email, bucket_name, response))
+             .format(group_email, bucket_name, response))
+
 
 def upload_to_gcp_bucket(bucket_name, destination_blob_name, source_file_name):
     """
@@ -97,7 +102,8 @@ def upload_to_gcp_bucket(bucket_name, destination_blob_name, source_file_name):
     try:
         # Ensure the source file exists
         if not os.path.exists(source_file_name):
-            raise FileNotFoundError("The source file {} does not exist.".format(source_file_name))
+            raise IOError(
+                "The source file {} does not exist.".format(source_file_name))
 
         # Get the bucket
         bucket = storage_client.get_bucket(bucket_name)
@@ -107,16 +113,17 @@ def upload_to_gcp_bucket(bucket_name, destination_blob_name, source_file_name):
 
         # Attempt to upload the file
         blob.upload_from_filename(source_file_name)
-    except FileNotFoundError as e:
-        # Handle the file not found error specifically
-        log.error("File not found: {}".format(e))
+    except IOError as e:
+        if e.errno == 2:
+            # Handle the file not found error specifically
+            log.error("File not found: {}".format(e))
         raise
     except GoogleCloudError as e:
         # Handle Google Cloud specific exceptions
         log.error("An error occurred with Google Cloud Storage: {}".format(e))
-        raise UploadError("Failed to upload {} to {}/{}".format(source_file_name,bucket_name, destination_blob_name))
+        raise UploadError("Failed to upload {} to {}/{}".format(
+            source_file_name, bucket_name, destination_blob_name))
     except Exception as e:
         # Handle any other exceptions
         log.error("An unexpected error occurred: {}".format(e))
         raise UploadError("Unexpected error during upload: {}".format(e))
-   
